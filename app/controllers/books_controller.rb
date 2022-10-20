c3e3a9b0d778de70db456a611bb6fcbe904b6ee8
osho210 <anysense.yamatsu@gmail.com>
2022-10-20 07:28:13 +0000

    バリデーションの実装まで完了

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,25 +1,48 @@
 class BooksController < ApplicationController
   def new
+    @books = Book.all.order(id: :asc)
+    @book = Book.new(book_params)
   end
 
   def index
     # dbに格納されているデータをすべて取得
-    @books = Book.all
+    @books = Book.all.order(id: :asc)
+    # 最新の投稿のデータをインスタンス変数に格納
     @book = Book.new
   end
 
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to '/'
+    @books = Book.all.order(id: :asc)
+    @book = Book.new(book_params)
+
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
+  def update
+    @book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
+  end
+
+private
   def book_params
     params.require(:book).permit(:title, :body)
   end
